4bf61e97872e7e7a933672df848fb9f5fff4705a
Ankit <24ankitpal@gmail.com>
2021-02-25 19:16:32 +0530

    Adding one more info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latititude,longitude,callback) => {
                 callback("Unable to find location",undefined)
             }        
             else{
-                callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. '+'There is a '+body.current.precip+'% '+' chance of rain.')
+                callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. '+'There is a '+body.current.precip+'% '+' chance of rain.'+'Wind Speed is '+body.current.wind_speed)
             }
         })
 }
